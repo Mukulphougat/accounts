05a294ee2d17d00a4a128d83184d4fbebf7f357a
mukulphogat <147586130+mukulphogat@users.noreply.github.com>
2024-11-19 00:01:14 +0530

    update server_start file-1

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -Dserver.port=80 accounts-0.0.1-SNAPSHOT.jar > /home/ec2-user/server/logs/app.log 2>&1 < /dev/null &
+sudo java -jar -Dserver.port=80 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
